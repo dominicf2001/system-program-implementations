34fe307102273721e216ffbe3720d0538e4bb191
dominic <dominicf2001@gmail.com>
2024-02-03 14:25:06 -0500

    Track which watched users are online

--- watch/watch.c
+++ watch/watch.c
@@ -4,22 +4,34 @@ Date: 02/03/2024
 watch.txt
 */
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <utmp.h>
+#include <string.h>
 #include "./utmplib.h"
 
-const char** online_lognames;
-const char** watched_lognames;
+const char **online_lognames;
+const char **watched_lognames;
+      int   *online_watched_lognames;
 
-void lognames_initialize(const char* lognames[], int size) {
+int lognames_includes(const char** lognames, const char* logname, const size_t size) {
+    for (size_t i = 0; i < size; ++i) {
+        if (strcmp(lognames[i], logname) == 0){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void lognames_initialize(const char* lognames[], const size_t size) {
     for (int i = 0; i < size; ++i) {
         lognames[i] = NULL;
     }
 }
 
-void lognames_print(const char* lognames[], const int size) {
+void lognames_print(const char* lognames[], const size_t size) {
     for (int i = 0; i < size; ++i){
         if (lognames[i])
         printf("%s ", lognames[i]);
@@ -27,7 +39,7 @@ void lognames_print(const char* lognames[], const int size) {
     printf("Are currently logged in\n");
 }
 
-int is_number(char* str){
+int is_number(const char* str){
     int i = 0;
     while (str[i] != '\0') {
         if (str[i] < '0' || '9' < str[i]) {
@@ -76,19 +88,30 @@ int main (int argc, char* argv[]) {
         atoi(argv[1]) :
         300;
 
-    const int SIZE = is_number(argv[1]) ?
+    const size_t SIZE = is_number(argv[1]) ?
         argc - 2 :
         argc - 1;
 
     // Initialize lognames and do first print
     watched_lognames = malloc(sizeof(char*) * SIZE);
     int offset = is_number(argv[1]) ? 2 : 1;
-    for (int i = offset, j = 0; i < argc; ++i, ++j) {
+    for (size_t i = offset, j = 0; i < argc; ++i, ++j) {
         watched_lognames[j] = argv[i];
     }
     int count = lognames_get_online();
     lognames_print(online_lognames, count);
-    lognames_print(watched_lognames, SIZE);
+
+    online_watched_lognames = malloc(sizeof(int) * SIZE);
+
+    for (int i = 0; i < SIZE; ++i) {
+        const char* watched_logname = watched_lognames[i];
+        int is_online = 0;
+        if (lognames_includes(online_lognames, watched_logname, count)){
+            is_online = 1;
+        }
+        online_watched_lognames[i] = is_online;
+    }
 
     free(watched_lognames);
+    free(online_lognames);
 }
