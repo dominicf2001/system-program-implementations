6fa4502db7467fbafd8a90bd0568b7ddf0be3c9d
dominic <dominicf2001@gmail.com>
2024-02-12 13:42:58 -0500

    do some basic flag checking

--- sttyl/sttyl.c
+++ sttyl/sttyl.c
@@ -4,6 +4,10 @@
 #include <fcntl.h>
 #include <termios.h>
 
+//int flags[3][3] = {
+//    {} // cflags
+//};
+
 int main() {
     int fd;
     if ((fd = open("/dev/tty1" , O_RDWR)) == -1) {
@@ -15,7 +19,25 @@ int main() {
     
     tcgetattr(fd, options);
 
-    printf("ospeed: %d\n", options->c_ospeed);
+    speed_t speed = cfgetospeed(options);
+    printf("speed %d baud; ", speed);
+
+    // c flags
+    int is_parenb = options->c_cflag & PARENB;
+    if (!is_parenb){
+        printf("-");
+    }
+    printf("parenb ");
+
+    int is_icanon = options->c_cflag & ICANON;
+    if (!is_icanon){
+        printf("-");
+    }
+    printf("icanon ");
+
+
+    
+    printf("\n");
 
     close(fd);
 }
