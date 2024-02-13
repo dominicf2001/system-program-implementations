cd2ed7b0aa0d1946494ddc8269e3f1618856b3e0
dominic <dominicf2001@gmail.com>
2024-02-12 19:41:29 -0500

    add some error handling and minor fixes

--- sttyl/sttyl.c
+++ sttyl/sttyl.c
@@ -13,32 +13,43 @@ struct flag {
     tcflag_t flag;
 };
 
+struct cc {
+    const char* c_name;
+    cc_t c_cc;
+};
+
 #define FLAG_NUM 12
 struct flag flags[FLAG_NUM] = {
-    {cflag, "parenb", PARENB},
-    {cflag, "icanon", ICANON},
-    {cflag, "hupcl", HUPCL},
-    {iflag, "brkint", BRKINT},
-    {iflag, "inpck", INPCK},
-    {iflag, "icnrl", ICRNL},
-    {iflag, "ixany", IXANY},
-    {oflag, "onlcr", ONLCR},
-    {lflag, "iexten", IEXTEN},
-    {lflag, "echo", ECHO},
-    {lflag, "echoe", ECHOE},
-    {lflag, "echok", ECHOK}
+    {cflag, "parenb", PARENB}, {cflag, "icanon", ICANON},
+    {cflag, "hupcl", HUPCL},   {iflag, "brkint", BRKINT},
+    {iflag, "inpck", INPCK},   {iflag, "icnrl", ICRNL},
+    {iflag, "ixany", IXANY},   {oflag, "onlcr", ONLCR},
+    {lflag, "iexten", IEXTEN}, {lflag, "echo", ECHO},
+    {lflag, "echoe", ECHOE},   {lflag, "echok", ECHOK}};
+
+#define CC_NUM 3
+struct cc ccs[CC_NUM] = {
+    {"intr", VINTR},
+    {"erase", VERASE},
+    {"kill", VKILL}
 };
 
 int flag_is_enabled(struct flag*, struct termios*);
-struct flag *flag_find(struct flag*, const char *);
+struct flag *flag_find(struct flag *, const char *);
+struct cc *cc_find(struct cc*, const char *);
 void flags_print(struct flag*, enum flag_type, struct termios*);
 
 int main(int argc, char *argv[]) {
     struct termios *options = malloc(sizeof(struct termios));
 
-    if (argc == 1) {
-        tcgetattr(STDIN_FILENO, options);
+    if (!isatty(STDIN_FILENO)){
+        fprintf(stderr, "STDIN is not a terminal\n");
+        return -1;
+    }
+
+    tcgetattr(STDIN_FILENO, options);
     
+    if (argc == 1) {
         speed_t speed = cfgetospeed(options);
         printf("speed %d baud; ", speed);
 
@@ -58,66 +69,62 @@ int main(int argc, char *argv[]) {
     }
     else {
         for (int i = 1; i < argc; ++i){
-            int flag_disabled = argv[i][0] == '-';
+            int option_disabled = (argv[i][0] == '-');
             
-            char* flag_name = malloc(sizeof(char) * (strlen(argv[i]) - flag_disabled));
-            strcpy(flag_name, argv[i] + flag_disabled);
+            char* option_name = malloc(sizeof(char) * (strlen(argv[i]) - option_disabled));
+            strcpy(option_name, argv[i] + option_disabled);
 
-            printf("name: %s\n", flag_name);
-            struct flag* flag = NULL;
-            if ((flag = flag_find(flags, flag_name)) == NULL){
+            printf("Option name: %s\n", option_name);
+
+            struct flag* flag = flag_find(flags, option_name);
+            struct cc* cc = cc_find(ccs, option_name);
+            
+            int is_cc = !flag && cc;
+            int is_flag = flag && !cc;
+            
+            if (!is_cc && !is_flag){
                 printf("Unknown mode\n");
                 return -1;
             }
 
-            tcsetattr(STDIN_FILENO, TCSANOW, options);int main(int argc, char *argv[]) {
-    struct termios *options = malloc(sizeof(struct termios));
-
-    if (argc == 1) {
-        tcgetattr(STDIN_FILENO, options);
-    
-        speed_t speed = cfgetospeed(options);
-        printf("speed %d baud; ", speed);
-
-        // print cflags
-        flags_print(flags, cflag, options);
-        printf("\n");
-
-        // print iflags
-        flags_print(flags, iflag, options);
-        // print oflags
-        flags_print(flags, oflag, options);
-        printf("\n");
-
-        // print lflags
-        flags_print(flags, lflag, options);    
-        printf("\n");   
-    }
-    else {
-        for (int i = 1; i < argc; ++i){
-            int flag_disabled = argv[i][0] == '-';
+            if (is_flag){
+                switch (flag->flag_type){
+                case cflag:
+                    if (option_disabled)
+                        options->c_cflag &= ~flag->flag;
+                    else 
+                        options->c_cflag |= flag->flag;
+                    break;
+                case iflag:
+                    if (option_disabled)
+                        options->c_iflag &= ~flag->flag;
+                    else
+                        options->c_iflag |= flag->flag;
+                    break;
+                case oflag:
+                    if (option_disabled)
+                        options->c_oflag &= ~flag->flag;
+                    else
+                        options->c_oflag |= flag->flag;
+                    break;
+                case lflag:
+                    if (option_disabled)
+                        options->c_lflag &= ~flag->flag;
+                    else
+                        options->c_lflag |= flag->flag;
+                    break;
+                }
+            }
             
-            char* flag_name = malloc(sizeof(char) * (sizeof(argv[i]) - flag_disabled));
-            strcpy(flag_name, argv[i] + flag_disabled);
-            
-            struct flag* flag = NULL;
-            if ((flag = flag_find(flags, flag_name)) == NULL){
-                printf("Unknown mode\n");
+            if (tcsetattr(STDIN_FILENO, TCSANOW, options) == -1){
+                perror("tcsetattr");
                 return -1;
             }
 
-            tcsetattr(STDIN_FILENO, TCSANOW, options);
-            free(flag_name);
+            free(option_name);
         }
     }
-    
-    free(options);
-}
-        }
-    }
-    
-    free(options);
-}
+}  
 
 int flag_is_enabled(struct flag *flag, struct termios *options){
     int flag_enabled = 0;
@@ -145,6 +152,16 @@ struct flag* flag_find(struct flag* flags, const char* flag_name) {
     for (int i = 0; i < FLAG_NUM; ++i){
         if (strcmp(flags[i].flag_name, flag_name) == 0){
             return &flags[i];
+        }
+    }
+    return NULL;
+}
+
+struct cc *cc_find(struct cc* ccs, const char * c_name) {
+    for (int i = 0; i < CC_NUM; ++i){
+        struct cc cc = ccs[i];
+        if (strcmp(cc.c_name, c_name) == 0){
+            return &ccs[i];
         }
     }
     return NULL;
